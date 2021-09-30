d46511489794384a3b7db8127f3e238baa81e7a9
kasv-p <84275697+kasv-p@users.noreply.github.com>
2021-09-30 23:21:37 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<style>
+body {
+/* background-image: url('https://i.pinimg.com/originals/f4/52/a2/f452a2f4b634b3011e065da8eaf0a5c3.gif');*/
+  background-image: url('https://www.uniper.energy/sites/default/files/gif%20home_reduced.gif');
+  background-image: url('https://i.pinimg.com/originals/3d/08/e0/3d08e03cb40252526fee2036a67f07f1.gif');
+  background-image: url('https://thumbs.gfycat.com/SneakyUnripeAssassinbug-size_restricted.gif');
+  background-size: 300px 1000px;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  font-family: Arial;
+}
+</style>
+  <head>
+    <meta charset="utf-8">
+    <title>login</title>
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  </head>
+  <body>
+    <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <div class="box" >
+      <h3 class="login"><em>LOGIN</em></h3>
+      <form action="index.html" method="post">
+        <tr>
+          <td class="input"><i class="material-icons">email</i>Username :</td>
+          <td><input type="text" name="username" class="text"><br><br></td>
+        </tr>
+        <tr>
+          <td class="input">Password &nbsp:</td>
+          <td><input class="text" type="password" name="pass"><br><br><br></td>
+        </tr>
+        <tr>
+          <td><input  class="button" type="submit" name="submit" value="Submit"></td>
+        </tr>
+      </form>
+  </body>
+</html>
